fd86bac328a2827fa31f6c757255ee55b1239248
Joshua Cho <j8cho@uwaterloo.ca>
2026-05-25 14:48:32 -0400

    Include vector in costmap_core.hpp
    Added vector header include for CostmapCore class.

--- src/robot/costmap/src/costmap_core.hpp
+++ src/robot/costmap/src/costmap_core.hpp
@@ -3,13 +3,14 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "nav_msgs/msg/occupancy_grid.hpp"
+#include <vector>
 
 namespace robot {
 
 class CostmapCore {
 public:
   CostmapCore(const rclcpp::Logger& logger);
-  
+
   void initializeCostmap();
   void convertToGrid(double range, double angle, int& x_grid, int& y_grid);
   void markObstacle(int x_grid, int y_grid);
