08b78a7898bb6849d7d1a96e0bf1f78212a828a3
Joshua Cho <j8cho@uwaterloo.ca>
2026-05-25 15:32:22 -0400

    Add integrateCostmap method to MapMemoryCore
    Added integrateCostmap method to MapMemoryCore class.

--- src/robot/map_memory/include/map_memory_core.hpp
+++ src/robot/map_memory/include/map_memory_core.hpp
@@ -2,6 +2,7 @@
 #define MAP_MEMORY_CORE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
 
 namespace robot
 {
@@ -9,9 +10,15 @@ namespace robot
 class MapMemoryCore {
   public:
     explicit MapMemoryCore(const rclcpp::Logger& logger);
+    void integrateCostmap(
+      const nav_msgs::msg::OccupancyGrid& costmap, 
+      nav_msgs::msg::OccupancyGrid& globalmap, 
+      double robot_x, 
+      double robot_y,
+      double robot_yaw);
 
   private:
-    rclcpp::Logger logger_;
+    rclcpp::Logger logger_;   
 };
 
 }  
