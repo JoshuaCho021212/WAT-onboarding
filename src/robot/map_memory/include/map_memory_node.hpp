5eddb973bb46c0a61040b490ad3b20a2177be870
Joshua Cho <j8cho@uwaterloo.ca>
2026-05-27 17:41:29 -0400

    Update map_memory_node.hpp

--- src/robot/map_memory/include/map_memory_node.hpp
+++ src/robot/map_memory/include/map_memory_node.hpp
@@ -1,22 +1,25 @@
 #ifndef MAP_MEMORY_NODE_HPP_
 #define MAP_MEMORY_NODE_HPP_
-
 #include "rclcpp/rclcpp.hpp"
 #include "nav_msgs/msg/occupancy_grid.hpp"
 #include "nav_msgs/msg/odometry.hpp"
-
 #include "map_memory_core.hpp"
-
 class MapMemoryNode : public rclcpp::Node {
   public:
     MapMemoryNode();
-
   private:
     robot::MapMemoryCore map_memory_;
     rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_sub_;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
     rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr map_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
+    nav_msgs::msg::OccupancyGrid latest_costmap_;
+    bool costmap_received_ = false;
+    double robot_x_ = 0.0, robot_y_ = 0.0, robot_yaw_ = 0.0;
+    double last_x_ = 0.0, last_y_ = 0.0;
+    bool should_update_ = false;
+    void costmapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg);
+    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
+    void updateMap();
 };
-
-#endif 
+#endif
