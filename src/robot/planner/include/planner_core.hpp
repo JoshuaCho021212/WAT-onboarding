de7beee2432f5c4e121e4bad46b12ab028d863ec
Joshua Cho <j8cho@uwaterloo.ca>
2026-05-27 19:27:30 -0400

    Update planner_core.hpp

--- src/robot/planner/include/planner_core.hpp
+++ src/robot/planner/include/planner_core.hpp
@@ -2,13 +2,69 @@
 #define PLANNER_CORE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "geometry_msgs/msg/point_stamped.hpp"
+#include "geometry_msgs/msg/pose.hpp"
 
 namespace robot
 {
 
+struct CellIndex
+{
+  int x;
+  int y;
+ 
+  CellIndex(int xx, int yy) : x(xx), y(yy) {}
+  CellIndex() : x(0), y(0) {}
+ 
+  bool operator==(const CellIndex &other) const
+  {
+    return (x == other.x && y == other.y);
+  }
+ 
+  bool operator!=(const CellIndex &other) const
+  {
+    return (x != other.x || y != other.y);
+  }
+};
+ 
+// Hash function for CellIndex so it can be used in std::unordered_map
+struct CellIndexHash
+{
+  std::size_t operator()(const CellIndex &idx) const
+  {
+    // A simple hash combining x and y
+    return std::hash<int>()(idx.x) ^ (std::hash<int>()(idx.y) << 1);
+  }
+};
+ 
+// Structure representing a node in the A* open set
+struct AStarNode
+{
+  CellIndex index;
+  double f_score;  // f = g + h
+ 
+  AStarNode(CellIndex idx, double f) : index(idx), f_score(f) {}
+};
+ 
+// Comparator for the priority queue (min-heap by f_score)
+struct CompareF
+{
+  bool operator()(const AStarNode &a, const AStarNode &b)
+  {
+    // We want the node with the smallest f_score on top
+    return a.f_score > b.f_score;
+  }
+};
+
 class PlannerCore {
   public:
     explicit PlannerCore(const rclcpp::Logger& logger);
+    nav_msgs::msg::Path planPath(
+          const nav_msgs::msg::OccupancyGrid& map,
+          const geometry_msgs::msg::Pose& start,
+          const geometry_msgs::msg::PointStamped& goal);
 
   private:
     rclcpp::Logger logger_;
