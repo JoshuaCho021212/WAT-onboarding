352b975a2d82955ed12fe421ca737f6e48e52b6f
Joshua Cho <j8cho@uwaterloo.ca>
2026-05-27 18:58:52 -0400

    Update planner_node.hpp

--- src/robot/planner/include/planner_node.hpp
+++ src/robot/planner/include/planner_node.hpp
@@ -4,6 +4,11 @@
 #include "rclcpp/rclcpp.hpp"
 
 #include "planner_core.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp" 
+#include "nav_msgs/msg/odometry.hpp"       
+#include "nav_msgs/msg/path.hpp"           
+#include "geometry_msgs/msg/point_stamped.hpp" 
+#include "geometry_msgs/msg/pose.hpp" 
 
 class PlannerNode : public rclcpp::Node {
   public:
@@ -11,6 +16,26 @@ class PlannerNode : public rclcpp::Node {
 
   private:
     robot::PlannerCore planner_;
+    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr map_sub_;
+    rclcpp::Subscription<geometry_msgs::msg::PointStamped>::SharedPtr goal_point_sub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    enum class State { WAITING_FOR_GOAL, WAITING_FOR_ROBOT_TO_REACH_GOAL };
+    State state_ = State::WAITING_FOR_GOAL;
+
+    nav_msgs::msg::OccupancyGrid current_map_;
+    geometry_msgs::msg::PointStamped goal_;
+    geometry_msgs::msg::Pose robot_pose_;
+    bool goal_received_ = false;
+
+    void mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg);
+    void goalCallback(const geometry_msgs::msg::PointStamped::SharedPtr msg);
+    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
+    void timerCallback();
+    void planPath();
+
 };
 
 #endif 
