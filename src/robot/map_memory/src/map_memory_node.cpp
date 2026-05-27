3b6bdfa947fe7ae4b23317b253593dd1d11b3172
Joshua Cho <j8cho@uwaterloo.ca>
2026-05-27 17:42:25 -0400

    Update map_memory_node.cpp

--- src/robot/map_memory/src/map_memory_node.cpp
+++ src/robot/map_memory/src/map_memory_node.cpp
@@ -1,25 +1,14 @@
 #include "map_memory_node.hpp"
+#include <cmath>
 
 MapMemoryNode::MapMemoryNode() : Node("map_memory"), map_memory_(robot::MapMemoryCore(this->get_logger())) {
-  
   costmap_sub_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>(
       "/costmap", 10, std::bind(&MapMemoryNode::costmapCallback, this, std::placeholders::_1));
-  
   odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
       "/odom/filtered", 10, std::bind(&MapMemoryNode::odomCallback, this, std::placeholders::_1));
-  
   map_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("/map", 10);
-  
   timer_ = this->create_wall_timer(
       std::chrono::seconds(1), std::bind(&MapMemoryNode::updateMap, this));
-
-  global_map_.header.frame_id = "sim_world";
-  global_map_.info.resolution = 0.1;
-  global_map_.info.width = 200;
-  global_map_.info.height = 200;
-  global_map_.info.origin.position.x = -10.0;
-  global_map_.info.origin.position.y = -10.0;
-  global_map_.data.assign(200 * 200, -1);
 }
 
 void MapMemoryNode::costmapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg) {
@@ -30,12 +19,10 @@ void MapMemoryNode::costmapCallback(const nav_msgs::msg::OccupancyGrid::SharedPt
 void MapMemoryNode::odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg) {
   double x = msg->pose.pose.position.x;
   double y = msg->pose.pose.position.y;
-
   double distance = std::sqrt(std::pow(x - last_x_, 2) + std::pow(y - last_y_, 2));
   if (distance >= 1.5) {
     robot_x_ = x;
     robot_y_ = y;
-
     auto q = msg->pose.pose.orientation;
     robot_yaw_ = std::atan2(2.0 * (q.w * q.z + q.x * q.y),
                             1.0 - 2.0 * (q.y * q.y + q.z * q.z));
@@ -53,8 +40,7 @@ void MapMemoryNode::updateMap() {
   }
 }
 
-int main(int argc, char ** argv)
-{
+int main(int argc, char ** argv) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MapMemoryNode>());
   rclcpp::shutdown();
