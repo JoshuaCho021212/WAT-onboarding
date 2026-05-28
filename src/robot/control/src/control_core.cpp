3bdbe00a8bbba1ca6c7eaab6f1e156ba417220c4
Joshua Cho <j8cho@uwaterloo.ca>
2026-05-27 22:28:00 -0400

    Update control_core.cpp

--- src/robot/control/src/control_core.cpp
+++ src/robot/control/src/control_core.cpp
@@ -1,4 +1,5 @@
 #include "control_core.hpp"
+#include <cmath>
 
 namespace robot
 {
@@ -13,7 +14,53 @@ namespace robot
 
   geometry_msgs::msg::Twist ControlCore::computeVelocity() {
     geometry_msgs::msg::Twist cmd;
+    if(!current_path_ || current_path_->poses.empty()) {
     return cmd;
+    }
+
+    if(!current_odometry_) {
+      return cmd;
+    }
+
+  auto& robot_pos = current_odometry_->pose.pose.position;
+  auto& goal_pos  = current_path_->poses.back().pose.position;
+  double dx = goal_pos.x - robot_pos.x;
+  double dy = goal_pos.y - robot_pos.y;
+  double dist = std::sqrt(dx*dx + dy*dy);
+    
+  if(dist < goal_tolerance_) {
+      return cmd;
+  }
+
+  geometry_msgs::msg::Point lookahead_point;
+  for(const auto& pose : current_path_->poses) {
+    double ldx = pose.pose.position.x - robot_pos.x;
+    double ldy = pose.pose.position.y - robot_pos.y;
+    double ldist = std::sqrt(ldx*ldx + ldy*ldy);
+    
+    if(ldist >= lookahead_distance_) {
+        lookahead_point = pose.pose.position;
+        break; 
+    }
+  }
+
+  auto& quat = current_odometry_->pose.pose.orientation;
+
+  double yaw = std::atan2(
+    2.0 * (quat.w * quat.z + quat.x * quat.y),
+    1.0 - 2.0 * (quat.y * quat.y + quat.z * quat.z));
+
+  double pdx = lookahead_point.x - robot_pos.x;
+  double pdy = lookahead_point.y - robot_pos.y;
+
+  double local_y = -(pdx) * std::sin(yaw) + (pdy) * std::cos(yaw);
+
+  double L = std::sqrt(pdx*pdx + pdy*pdy);
+  double curvature = 2.0 * local_y / (L * L);
+  cmd.linear.x = linear_speed_;
+  cmd.angular.z = linear_speed_ * curvature;
+    
+  return cmd;
   }
 
 ControlCore::ControlCore(const rclcpp::Logger& logger) 
