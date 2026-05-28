d2ac7c7722385b2ebc90e6e88cfe78999bc74eeb
Joshua Cho <j8cho@uwaterloo.ca>
2026-05-28 12:09:14 -0400

    Update costmap_core.hpp

--- src/robot/costmap/include/costmap_core.hpp
+++ src/robot/costmap/include/costmap_core.hpp
@@ -2,20 +2,30 @@
 #define COSTMAP_CORE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include <vector>
 
-namespace robot
-{
+namespace robot {
 
 class CostmapCore {
-  public:
-    // Constructor, we pass in the node's RCLCPP logger to enable logging to terminal
-    explicit CostmapCore(const rclcpp::Logger& logger);
+public:
+  CostmapCore(const rclcpp::Logger& logger);
 
-  private:
-    rclcpp::Logger logger_;
+  void initializeCostmap();
+  void convertToGrid(double range, double angle, int& x_grid, int& y_grid);
+  void markObstacle(int x_grid, int y_grid);
+  void inflateObstacles();
+  nav_msgs::msg::OccupancyGrid publishCostmap();
 
+private:
+  rclcpp::Logger logger_;
+  nav_msgs::msg::OccupancyGrid costmap_;
+  std::vector<std::vector<int>> grid_;
+  int width_ = 100;
+  int height_ = 100;
+  double resolution_ = 0.1;
 };
 
-}  
+}
 
-#endif  
+#endif
