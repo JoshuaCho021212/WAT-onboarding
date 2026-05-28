ae2b6195762e94295a16a8ad39c1b603f4a6b19c
Joshua Cho <j8cho@uwaterloo.ca>
2026-05-27 21:42:14 -0400

    Update control_core.cpp

--- src/robot/control/src/control_core.cpp
+++ src/robot/control/src/control_core.cpp
@@ -3,7 +3,24 @@
 namespace robot
 {
 
+  void ControlCore::setPath(nav_msgs::msg::Path::SharedPtr msg) {
+    current_path_ = msg;
+  }
+
+  void ControlCore::setOdom(nav_msgs::msg::Odometry::SharedPtr msg) {
+    current_odometry_ = msg;
+  }
+
+  geometry_msgs::msg::Twist ControlCore::computeVelocity() {
+    geometry_msgs::msg::Twist cmd;
+    return cmd;
+  }
+
 ControlCore::ControlCore(const rclcpp::Logger& logger) 
-  : logger_(logger) {}
+  : logger_(logger) {
+    lookahead_distance_ = 1.0;
+    goal_tolerance_     = 0.1;
+    linear_speed_       = 0.5;
+  }
 
 }  
