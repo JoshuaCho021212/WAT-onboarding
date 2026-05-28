d967bf5fa05cd302705cbd111ff60e7b16d3bb27
Joshua Cho <j8cho@uwaterloo.ca>
2026-05-27 21:29:00 -0400

    Update control_core.hpp

--- src/robot/control/include/control_core.hpp
+++ src/robot/control/include/control_core.hpp
@@ -2,6 +2,9 @@
 #define CONTROL_CORE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 
 namespace robot
 {
@@ -10,11 +13,16 @@ class ControlCore {
   public:
     // Constructor, we pass in the node's RCLCPP logger to enable logging to terminal
     ControlCore(const rclcpp::Logger& logger);
+    void setPath(nav_msgs::msg::Path::SharedPtr);
+    void setOdom(nav_msgs::msg::Odometry::SharedPtr);
+    geometry_msgs::msg::Twist computeVelocity();
   
   private:
     rclcpp::Logger logger_;
+    nav_msgs::msg::Path::SharedPtr current_path_;
+    nav_msgs::msg::Odometry::SharedPtr current_odometry_;
+    double lookahead_distance_, goal_tolerance_, linear_speed_ = 0.0;
 };
-
 } 
 
 #endif 
